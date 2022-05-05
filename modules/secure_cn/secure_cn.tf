c00be22a99d7edc3da02571f26a87079ae65e08a
emcnicholas <emcnicholas@comast.net>
2022-05-05 09:30:41 -0400

    Deploy Env

--- modules/secure_cn/secure_cn.tf
+++ modules/secure_cn/secure_cn.tf
@@ -1,34 +1,34 @@
 // SecureCN Cluster
 
-#resource "securecn_k8s_cluster" "cluster" {
-#  kubernetes_cluster_context = var.kubernetes_cluster_context_name
-#  name = var.environment_name
-#  orchestration_type = "EKS"
-#  ci_image_validation = false
-#  cd_pod_template = false
-#  connections_control = true
-#  inspect_incoming_cluster_connections = false
-#  multi_cluster_communication_support = false
-#  fail_close = false
-#  persistent_storage = false
-#  trace_analyzer = true
-#  api_intelligence_dast = true
-#}
-#
-#// SecureCN Environments
-#resource "securecn_environment" "yelb" {
-#
-#  name = "${var.environment_name}_Yelb_App"
-#  description = "Yelb App Environment"
-#
-#  kubernetes_environment {
-#    cluster_name = securecn_k8s_cluster.cluster.name
-#
-#    namespaces_by_labels = {
-#      app = "yelb"
-#    }
-#  }
-#}
+resource "securecn_k8s_cluster" "cluster" {
+  kubernetes_cluster_context = var.kubernetes_cluster_context_name
+  name = var.environment_name
+  orchestration_type = "EKS"
+  ci_image_validation = false
+  cd_pod_template = false
+  connections_control = true
+  inspect_incoming_cluster_connections = false
+  multi_cluster_communication_support = false
+  fail_close = false
+  persistent_storage = false
+  # trace_analyzer = true
+  # api_intelligence_dast = true
+}
+
+// SecureCN Environments
+resource "securecn_environment" "yelb" {
+
+  name = "${var.environment_name}_Yelb_App"
+  description = "Yelb App Environment"
+
+  kubernetes_environment {
+    cluster_name = securecn_k8s_cluster.cluster.name
+
+    namespaces_by_labels = {
+      app = "yelb"
+    }
+  }
+}
 
 #// SecureCN Connection Rules
 #
