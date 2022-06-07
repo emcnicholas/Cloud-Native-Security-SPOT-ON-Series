2d5ccd33b02efffa44dd335173acec2143f64f94
emcnicholas <emcnicholas@comast.net>
2022-06-07 10:34:50 -0400

    Deploy Env

--- modules/panoptica/panoptica.tf
+++ modules/panoptica/panoptica.tf
@@ -30,43 +30,51 @@ resource "securecn_environment" "yelb" {
   }
 }
 
-#// SecureCN Connection Rules
-#
-#// Yelb Connection Rules
-#resource "securecn_connection_rule" "External_to_Yelb_UI" {
-#  rule_name = "External to Yelb Web UI "
-#  source_by_external = true
-#  destination_by_pod_name {
-#    names = ["yelb-ui"]
-#  }
-#}
-#
-#resource "securecn_connection_rule" "Yelb_UI_to_App" {
-#  rule_name = "Yelb UI to App"
-#  source_by_pod_name {
-#    names = ["yelb-ui"]
-#  }
-#  destination_by_pod_name {
-#    names = ["yelb-appserver"]
-#  }
-#}
-#
-#resource "securecn_connection_rule" "Yelb_App_to_DB" {
-#  rule_name = "Yelb App to DB"
-#  source_by_pod_name {
-#    names = ["yelb-appserver"]
-#  }
-#  destination_by_pod_name {
-#    names = ["yelb-db"]
-#  }
-#}
-#
-#resource "securecn_connection_rule" "Yelb_App_to_Redis" {
-#  rule_name = "Yelb App to Redis"
-#  source_by_pod_name {
-#    names = ["yelb-appserver"]
-#  }
-#  destination_by_pod_name {
-#    names = ["redis-server"]
-#  }
-#}
+// Panoptica Connection Rules
+
+// Yelb Connection Rules
+resource "securecn_connection_rule" "External_to_Yelb_UI" {
+  rule_name = "External to Yelb Web UI "
+  source_by_external = true
+  destination_by_pod_name {
+    names = ["yelb-ui"]
+  }
+}
+
+resource "securecn_connection_rule" "Yelb_UI_to_App" {
+  rule_name = "Yelb UI to App"
+  source_by_pod_name {
+    names = ["yelb-ui"]
+  }
+  destination_by_pod_name {
+    names = ["yelb-appserver"]
+  }
+}
+
+resource "securecn_connection_rule" "Yelb_App_to_DB" {
+  rule_name = "Yelb App to DB"
+  source_by_pod_name {
+    names = ["yelb-appserver"]
+  }
+  destination_by_pod_name {
+    names = ["yelb-db"]
+  }
+}
+
+resource "securecn_connection_rule" "Yelb_App_to_Redis" {
+  rule_name = "Yelb App to Redis"
+  source_by_pod_name {
+    names = ["yelb-appserver"]
+  }
+  destination_by_pod_name {
+    names = ["redis-server"]
+  }
+}
+
+resource "securecn_connection_rule" "Any_to_External" {
+  rule_name = "Yelb Pods to External"
+  source_by_pod_any {
+
+  }
+  destination_by_external = true
+}
