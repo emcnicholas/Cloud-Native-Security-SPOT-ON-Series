6903230a0b00fa425e860d964a258532e6ec7d65
emcnicholas <emcnicholas@comast.net>
2022-06-07 08:28:01 -0400

    Deploy Env

--- main.tf
+++ main.tf
@@ -44,13 +44,13 @@ module "Secure_Cloud_Analytics" {
 
 
 // Deploy Secure Application Cloud (CN)
-#module "Panoptica" {
-#  depends_on                      = [null_resource.update_kubeconfig]
-#  source                          = "./modules/panoptica"
-#  environment_name                = module.Infrastructure.eks_cluster_name
-#  kubernetes_cluster_context_name = module.Infrastructure.eks_cluster_arn
-#}
-#
+module "Panoptica" {
+  depends_on                      = [null_resource.update_kubeconfig]
+  source                          = "./modules/panoptica"
+  environment_name                = module.Infrastructure.eks_cluster_name
+  kubernetes_cluster_context_name = module.Infrastructure.eks_cluster_arn
+}
+
 #// Deploy Secure Application (AppD) -  I know so confusing :(
 #module "Secure_App" {
 #  source = "./modules/secure_app"
